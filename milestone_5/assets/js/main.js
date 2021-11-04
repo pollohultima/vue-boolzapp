dd4b4b3e95cdb47e5dbbc1d7f7653258f35665fc
pollohultima <matteo.polonio@gmail.com>
2021-11-04 17:05:39 +0100

    Milestone 4 completed + initial push and first part milestone 5

---
+++ milestone_5/assets/js/main.js
@@ -0,0 +1,148 @@
+const app = new Vue({
+
+    el: '#root',
+
+    data: {
+
+        counter: 0,
+
+        i: 0,
+
+        input_text: "",
+
+        input_contact: "",
+
+        last_log: dayjs().format('H:mm:ss'),
+
+
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Ricordati di dargli da mangiare',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        text: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+                visible: true,
+                messages: [
+                    {
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '20/03/2020 16:30:55',
+                        text: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
+                    }
+                ],
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+                visible: true,
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Luisa',
+                avatar: '_6',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
+        ]
+    },
+
+    methods: {
+        changeContact(i) {
+            this.counter = i
+            console.log(this.counter);
+        },
+
+        reply() {
+            this.contacts[this.counter].messages.push({
+                date: dayjs().format('DD/MM/YYYY H:mm:ss'), text: 'ok', status: 'received'
+            })
+        },
+
+        sendMsg() {
+            if (this.input_text != "") {
+
+                this.contacts[this.counter].messages.push({
+                    date: dayjs().format('DD/MM/YYYY H:mm:ss'), text: this.input_text, status: 'sent'
+                }),
+
+                    this.input_text = "",
+
+                    setTimeout(this.reply, 1000)
+            }
+        },
+
+
+
+
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
